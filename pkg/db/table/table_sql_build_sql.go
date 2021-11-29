f419ebbd55476637e607e15d42c684a058d8b68d
wuxiaofeng <wuxiaofeng@querycap.com>
2021-11-29 14:47:03 +0800

    fix bug
    Signed-off-by: wuxiaofeng <wuxiaofeng@querycap.com>

--- pkg/db/table/table_sql_build_sql.go
+++ pkg/db/table/table_sql_build_sql.go
@@ -29,19 +29,23 @@ func (t *TableSql) Name() string {
 func (t *TableSql) CreateTable() string {
 	split := "\r\n\n"
 	table := make([]string, 0)
+	var alterSequence string
 	// create fields
 	table = append(table, t.Fields(func(s string, s2 string) {
-		seq := t.Sequence(s, s2)
+		seq, as := t.Sequence(s, s2)
 		if seq != "" {
 			table = append(table, seq)
 		}
+		alterSequence = as
 	}))
+	// alter seq
+	if alterSequence != "" {
+		table = append(table, alterSequence)
+	}
 	// alter fields
 	table = append(table, t.AlterColumn())
 	// create index
 	table = append(table, t.Index())
-	// alter index
-	table = append(table, t.AlterIndex())
 	return strings.Join(table, split)
 }
 
@@ -68,11 +72,10 @@ func (t *TableSql) Fields(seq func(string, string)) string {
 	return fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s ( %s );", t.Name(), define)
 }
 
-func (t *TableSql) Sequence(fieldName, seqName string) string {
-	seq := make([]string, 0)
-	seq = append(seq, fmt.Sprintf("CREATE SEQUENCE %s INCREMENT 1 MINVALUE 1 MAXVALUE %d START 1 CACHE 1;", seqName, math.MaxInt64))
-	seq = append(seq, fmt.Sprintf("ALTER SEQUENCE %s OWNED BY %s.%s;", seqName, t.Name(), fieldName))
-	return strings.Join(seq, "\n")
+func (t *TableSql) Sequence(fieldName, seqName string) (cs string, as string) {
+	cs = fmt.Sprintf("CREATE SEQUENCE IF NOT EXISTS %s INCREMENT 1 MINVALUE 1 MAXVALUE %d START 1 CACHE 1;", seqName, math.MaxInt64)
+	as = fmt.Sprintf("ALTER SEQUENCE %s OWNED BY %s.%s;", seqName, t.Name(), fieldName)
+	return
 }
 
 // Index /**
@@ -90,32 +93,6 @@ func (t *TableSql) Index() string {
 			ints := strings.Split(tmp.String(), "$")
 			// default method btree
 			indef := fmt.Sprintf("CREATE %s IF NOT EXISTS %s ON %s (%s);", ints[IndexCursorClass], ints[IndexCursorName], t.Name(), ints[IndexCursorFields])
-			indexs = append(indexs, indef)
-		}
-	}
-	return strings.Join(indexs, "\n")
-}
-
-// AlterIndex /**
-//ALTER INDEX [ IF EXISTS ] name RENAME TO new_name
-//ALTER INDEX [ IF EXISTS ] name SET TABLESPACE tablespace_name
-//ALTER INDEX name ATTACH PARTITION index_name
-//ALTER INDEX name DEPENDS ON EXTENSION extension_name
-//ALTER INDEX [ IF EXISTS ] name SET ( storage_parameter [= value] [, ... ] )
-//ALTER INDEX [ IF EXISTS ] name RESET ( storage_parameter [, ... ] )
-//ALTER INDEX [ IF EXISTS ] name ALTER [ COLUMN ] column_number
-//    SET STATISTICS integer
-//ALTER INDEX ALL IN TABLESPACE name [ OWNED BY role_name [, ... ] ]
-//    SET TABLESPACE new_tablespace [ NOWAIT ]
-//Description
-func (t *TableSql) AlterIndex() string {
-	indexs := make([]string, 0)
-	for i, _ := range t.Def.IndexDef {
-		if t.Def.IndexDef[i] != nil {
-			tmp := SqlTableIndexDef(*t.Def.IndexDef[i])
-			ints := strings.Split(tmp.String(), "$")
-			// default method btree
-			indef := fmt.Sprintf("ALTER %s IF NOT EXISTS %s ON %s (%s);", ints[IndexCursorClass], ints[IndexCursorName], t.Name(), ints[IndexCursorFields])
 			indexs = append(indexs, indef)
 		}
 	}
